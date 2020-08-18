0bd8fa690b7affc0516a9dd2b3e34a278215228d
MarcoDiFrancesco <marcodifran@gmail.com>
2020-08-18 14:40:52 +0200

    Update check low batter, add notes

--- scripts/check_low_battery.sh
+++ scripts/check_low_battery.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Source: https://unix.stackexchange.com/a/190155/337926
 
-TOO_LOW=10 # how low is too low?
-STATUS_NOT_CHARGING="Discharging"
 ICON="/home/marco/Documents/low-battery.svg" # eye candy
 
-#export DISPLAY=:0
-
 BATTERY_LEVEL=$(cat /sys/class/power_supply/BAT0/capacity)
-STATUS=$(cat /sys/class/power_supply/BAT0/status)
+TOO_LOW=100
 
-if [ $BATTERY_LEVEL -le $TOO_LOW -a $STATUS = $STATUS_NOT_CHARGING ]; then
-    /usr/bin/notify-send -u critical -i "$ICON" -t 5000 "Battery low" "Battery level is ${BATTERY_LEVEL}%!"
+STATUS=$(cat /sys/class/power_supply/BAT0/status)
+STATUS_NOT_CHARGING="Discharging"
+
+if [ $BATTERY_LEVEL -le $TOO_LOW -a "$STATUS" = $STATUS_NOT_CHARGING ]; then
+    # Send notification that lasts for 5 minutes
+    /usr/bin/notify-send -u critical -i "$ICON" -t 300000 "Battery low" "Battery level is ${BATTERY_LEVEL}%!"
 fi
 
 exit 0
