eaf822c946803058f75c20d4d92bbd45285c982e
MarcoDiFrancesco <marcodifran@gmail.com>
2021-11-07 13:17:34 +0100

    Update git ignore, polybar, keyboard, logkeys, vpn

--- .config/polybar/bluetooth.sh
+++ .config/polybar/bluetooth.sh
@@ -5,77 +5,79 @@ MAC_MOUSE=CA:C9:FC:48:31:81
 MAC_HEADPHONES=1C:52:16:3A:9A:65
 
 bluetooth_print() {
-    while true; do
-        if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
-            if [[ "$(bluetoothctl show | grep 'Powered: yes')" = *Powered:\ yes ]]; then
-                if [[ "$(bluetoothctl info $1 | grep "Connected: yes")" = *Connected:\ yes ]]; then
-                    # Connected
-                    printf "%s" ""
-                else
-                    # Disconnected
-                    printf "%s" "%{F#66ffffff}"
-                fi
-                if [ $1 = $MAC_MOUSE ]; then
-                    # Mouse
-                    printf "%s" ""
-                elif [ $1 = $MAC_HEADPHONES ]; then
-                    # Headphones
-                    printf "%s" ""
-                fi
-                printf '\n'
-            else
-                # Disconnected
-                # Print disconnected only in one device
-                if [ $1 = $MAC_MOUSE ]; then
-                    echo "%{F#66ffffff}"
-                elif [ $1 = $MAC_HEADPHONES ]; then
-                    echo ""
-                fi
-            fi
+  while true; do
+    if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
+      if [[ "$(bluetoothctl show | grep 'Powered: yes')" = *Powered:\ yes ]]; then
+        if [[ "$(bluetoothctl info $1 | grep "Connected: yes")" = *Connected:\ yes ]]; then
+          # Connected
+          printf "%s" ""
         else
-            echo "Bluetooth module disabled"
+          # Disconnected
+          printf "%s" "%{F#66ffffff}"
         fi
-        sleep 1 &
-        wait
-    done
+        if [ $1 = $MAC_MOUSE ]; then
+          # Mouse
+          printf "%s" ""
+        elif [ $1 = $MAC_HEADPHONES ]; then
+          # Headphones
+          printf "%s" ""
+        fi
+        printf '\n'
+      else
+        # Disconnected
+        # Print disconnected only in one device
+        if [ $1 = $MAC_MOUSE ]; then
+          echo "%{F#66ffffff}"
+        elif [ $1 = $MAC_HEADPHONES ]; then
+          echo ""
+        fi
+      fi
+    else
+      echo "Bluetooth module disabled"
+    fi
+    sleep 1 &
+    wait
+  done
 }
 
-toggle_device() {
-    # Avoiding cases when the bluetooth card is off
-    if [[ "$(bluetoothctl info $1 | grep "Connected: no")" = *Connected:\ no ]]; then
-        bluetoothctl connect $1
-    elif [[ "$(bluetoothctl info $1 | grep "Connected: yes")" = *Connected:\ yes ]]; then
-        bluetoothctl disconnect $1
-    fi
-}
+## Not used
+# toggle_device() {
+#     # Avoiding cases when the bluetooth card is off
+#     if [[ "$(bluetoothctl info $1 | grep "Connected: no")" = *Connected:\ no ]]; then
+#         bluetoothctl connect $1
+#     elif [[ "$(bluetoothctl info $1 | grep "Connected: yes")" = *Connected:\ yes ]]; then
+#         bluetoothctl disconnect $1
+#     fi
+# }
 
-toggle_bluetooth() {
-    if [[ "$(bluetoothctl show | grep 'Powered: no')" = *Powered:\ no ]]; then
-        bluetoothctl power on
-        echo "Bluetooth powered on"
-    elif [[ "$(bluetoothctl show | grep 'Powered: yes')" = *Powered:\ yes ]]; then
-        echo "Bluetooth already off"
-        bluetoothctl power off
-    fi
-}
+## Not used
+# toggle_bluetooth() {
+#     if [[ "$(bluetoothctl show | grep 'Powered: no')" = *Powered:\ no ]]; then
+#         bluetoothctl power on
+#         echo "Bluetooth powered on"
+#     elif [[ "$(bluetoothctl show | grep 'Powered: yes')" = *Powered:\ yes ]]; then
+#         echo "Bluetooth already off"
+#         bluetoothctl power off
+#     fi
+# }
 
 case "$1" in
-    --toggle-bluetooth)
-        toggle_bluetooth
-        ;;
-    --toggle-headphones)
-        toggle_device $MAC_HEADPHONES
-        ;;
-    --toggle-mouse)
-        toggle_device $MAC_MOUSE
-        ;;
-    --print-headphones)
-        bluetooth_print $MAC_HEADPHONES
-        ;;
-    --print-mouse)
-        bluetooth_print $MAC_MOUSE
-        ;;
-    *)
-        bluetooth_print
-        ;;
+--toggle-bluetooth)
+  toggle_bluetooth
+  ;;
+# --toggle-headphones)
+#     toggle_device $MAC_HEADPHONES
+#     ;;
+# --toggle-mouse)
+#     toggle_device $MAC_MOUSE
+#     ;;
+--print-headphones)
+  bluetooth_print $MAC_HEADPHONES
+  ;;
+--print-mouse)
+  bluetooth_print $MAC_MOUSE
+  ;;
+*)
+  bluetooth_print
+  ;;
 esac
--- .config/polybar/energy_mode.sh
+++ .config/polybar/energy_mode.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 get_energy_mode() {
-        while true; do
-        if [ "$(cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor | grep performance)" ]; then
-            # Performance color
-            printf "%s" ""
-        else
-            # Powersafe color
-            printf "%s" "%{F#66ffffff}"
-        fi
-        # Bolt icon
-        printf "%s" ""
-        printf '\n'
-        sleep 1 &
-        wait
-    done
+  while true; do
+    if [ "$(cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor | grep performance)" ]; then
+      # Performance color
+      printf "%s" ""
+    else
+      # Powersafe color
+      printf "%s" "%{F#66ffffff}"
+    fi
+    # Bolt icon
+    printf "%s" ""
+    printf '\n'
+    sleep 1 &
+    wait
+  done
 }
 
 case "$1" in
-    *)
-        get_energy_mode
-        ;;
+*)
+  get_energy_mode
+  ;;
 esac
