bb1a3cc0a4fdffb253a1169ac8a91339a9d4b7f4
MarcoDiFrancesco <marcodifran@gmail.com>
2022-06-02 13:04:04 +0200

    Update video preview ranger

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -93,7 +93,7 @@ handle_image() {
   ## Video
   video/*)
       # Thumbnail
-      ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+      ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 -t 0 && exit 6
       exit 1;;
 
   # PDF
